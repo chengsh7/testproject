3893a9558617e9bd479f739e691c8eb0f8daec31
chengsh7 <shek.hang.cheng@adv-fusionex.com>
2016-04-21 16:01:41 +0800

    Add print line

--- src/main/java/testproject/TestClass.java
+++ src/main/java/testproject/TestClass.java
@@ -1,5 +1,13 @@
 package testproject;
 
 public class TestClass {
+	private String testId;
 
+	public String getTestId() {
+		return testId;
+	}
+
+	public void setTestId(String testId) {
+		this.testId = testId;
+	}
 }
